94fc5fe7461123886620df9e0067187228f116ed
Ivan Kalinin <k.beaver.i03@mail.ru>
2024-05-05 19:55:30 +0300

    Добавьте файлы проекта.

---
+++ KG_5/Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KG_5
+{
+    public partial class Form1 : Form
+    {
+        float pictureBoxWidth;
+        float pictureBoxHeight;
+        Graphics g;
+        Pen axisPen = new Pen(Color.Black, 2);
+        Pen gridPen = new Pen(Color.Black, 0.5f);
+        Font Fon = new Font("Arial", 9, FontStyle.Regular);
+        Brush brush = new SolidBrush(Color.Black);
+        Brush fillArea = new SolidBrush(Color.Red);
+        float divX;
+        float divY;
+        const int countX = 20;
+        const int countY = 20;
+        float centerX, centerY;
+        List<PointF> points = new List<PointF>();
+        List<Section> sections = new List<Section>();
+        List<List<int>> area = new List<List<int>>();
+        List <Point> pointList = new List<Point>();
+        public Form1()
+        {
+            InitializeComponent();
+            g = pictureBox1.CreateGraphics();
+            pictureBoxWidth = pictureBox1.Width;
+            pictureBoxHeight = pictureBox1.Height;
+            divX = pictureBoxWidth / countX;
+            divY = pictureBoxHeight / countY;
+            centerX = pictureBoxWidth / 2;
+            centerY = pictureBoxHeight / 2;
+            for (int i = 0; i < countX+10; i++)
+            {
+                area.Add(new List<int>());
+                for (int j = 0; j < countY+10; j++)
+                {
+                    area[i].Add(0);
+                }
+            }
+        }
+
+        private void inputFile()
+        {
+            string line;
+            try
+            {
+                //Pass the file path and file name to the StreamReader constructor
+                StreamReader sr = new StreamReader("input.txt");
+                //Read the first line of text
+                line = sr.ReadLine();
+                //Continue to read until you reach end of file
+                while (line != null)
+                {
+                    string[] coordinates = line.Split(' ');
+                    float x = float.Parse(coordinates[0]);
+                    float y = float.Parse(coordinates[1]);
+                    points.Add(new PointF(x, y));
+                    line = sr.ReadLine();
+                }
+                sr.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            for (int i = 0;i < points.Count; i++)
+            {
+                sections.Add(new Section(points[i], points[(i+1)%points.Count]));
+            }
+        }
+        private List<PointF> Convert(List<PointF> point)
+        {
+            List<PointF> result = new List<PointF>();
+            for (int i = 0;i < point.Count; i++)
+            {
+                result.Add(Convert(point[i]));
+            }
+            return result;
+        }
+        private List<Point> Convert(List<Point> point)
+        {
+            List<Point> result = new List<Point>();
+            for (int i = 0; i < point.Count; i++)
+            {
+                result.Add(Convert(point[i]));
+            }
+            return result;
+        }
+        private PointF Convert(PointF point)
+        {
+            return new PointF(divX +(point.X* divX)  ,(pictureBoxHeight-divY)-point.Y*divY);
+        }
+        private Point Convert(Point point)
+        {
+            return new Point((int)(divX + (point.X * divX)), (int)((pictureBoxHeight - divY) - point.Y * divY));
+        }
+        private void drawAxis()
+        {
+            g.Clear(Color.White);
+            PointF axisXStart = new PointF(divX, pictureBoxHeight - divY);
+            PointF axisXEnd = new PointF(pictureBoxWidth, pictureBoxHeight - divY);
+            PointF axisYStart = new PointF(divX , 0);
+            PointF axisYEnd = new PointF(divX, pictureBoxHeight-divY);
+            g.DrawLine(axisPen, axisXStart, axisXEnd);
+            g.DrawLine(axisPen, axisYStart, axisYEnd);
+            for (int i = 1; i <= countY; i++)
+            {
+                g.DrawString((i).ToString(), Fon, brush, divX - 17, pictureBoxHeight + divY * -i - divY);
+            }
+            for (int i = 1; i <= countX; i++)
+            {
+                g.DrawString(i.ToString(), Fon, brush, divX * i+5, pictureBoxHeight -15);
+            }
+        }
+        private void drawGrid()
+        {
+            for (int i = 0;i <= countY ; i++)
+            {
+                g.DrawLine(gridPen, 0, divY * i, pictureBoxWidth, divY * i );
+            }
+            for (int i = 0; i <= countX; i++)
+            {
+                g.DrawLine(gridPen, divX*i ,0 , divX * i, pictureBoxHeight);
+            }
+        }
+        private void drawBorder()
+        {
+            for (int i = 0;i < sections.Count;i++)
+            {
+                List <PointF> tmp = Convert(sections[i].GetPixels());
+                List <PointF> tmp2 =sections[i].GetPixels();
+                for (int j = 0;j < tmp.Count; j++)
+                {
+                    area[(int)tmp2[j].X][(int)tmp2[j].Y] = 1;
+                    g.FillRectangle(brush, tmp[j].X, tmp[j].Y, divX, divY);
+                }
+            }
+        }
+
+        private void XOR()
+        {
+            bool condition = false;
+            for (int j = 1;j < countY + 10;j++)
+            {
+                condition = false;
+                for (int i = 1;i < countX+ 10; i++)
+                {
+         
+                    if ((area[i][j] ^ area[i-1][j]) == 1)
+                    {
+                        area[i][j] = 1;
+                        condition = true;
+                    }
+                    else if()
+
+                    else
+                    {
+                        condition = false;
+                        area[i][j] = 0;
+  
+                    }
+                }
+            }
+        }
+        private void drawFillArea()
+        {
+            for (int i = 0;i< area.Count-1; i++)
+            {
+                for (int j = 0; j < area[i].Count - 1; j++)
+                {
+                    if (area[i][j] == 1)
+                    {
+                        pointList.Add(new Point(i,j));
+
+                    }
+                }
+            }
+            pointList = Convert(pointList);
+            for (int i = 0;i < pointList.Count; i++)
+            {
+                g.FillRectangle(fillArea, pointList[i].X, pointList[i].Y, divX, divY);
+            }
+        }
+        private void outputFile1()
+        {
+            try
+            {
+                //Pass the filepath and filename to the StreamWriter Constructor
+                StreamWriter sw = new StreamWriter("output1.txt");
+                //Write a line of text
+                for (int i = 0;i< area.Count; i++)
+                {
+                    for (int j = 0;j< area[i].Count; j++)
+                    {
+                        sw.Write(area[i][j]);
+                    }
+                    sw.Write('\n');
+                }
+                //Close the file
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+        }
+        private void outputFile2()
+        {
+            try
+            {
+                //Pass the filepath and filename to the StreamWriter Constructor
+                StreamWriter sw = new StreamWriter("output2.txt");
+                //Write a line of text
+                for (int i = 0; i < area.Count; i++)
+                {
+                    for (int j = 0; j < area[i].Count; j++)
+                    {
+                        sw.Write(area[i][j]);
+                    }
+                    sw.Write('\n');
+                }
+                //Close the file
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            inputFile();
+            drawAxis();
+            drawGrid();
+            drawBorder();
+            outputFile2();
+            XOR();
+            drawFillArea();
+            outputFile1();
+            drawBorder();
+            //Section sec = new Section(new PointF(8, 8), new PointF(8, 1));
+            //List<PointF> tmp = Convert(sec.GetPixels());
+            //for (int i = 0; i < tmp.Count; i++)
+            //{
+            //    g.FillRectangle(brush, tmp[i].X, tmp[i].Y, divX, divY);
+            //}
+        }
+    }
+}
---
+++ KG_5/Section.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KG_5
+{
+    internal class Section
+    {
+        PointF P1;
+        PointF P2;
+        public
+        float centeredX;
+        float centeredY;
+        List<PointF> pixels;
+        public Section(PointF P1, PointF P2)
+        {
+            this.P1 = P1;
+            this.P2 = P2;
+            this.pixels = new List<PointF>();
+            transformations();
+        }
+
+        private void transformations()
+        {
+            bool InvertX = false;
+            bool InvertY = false;
+            bool InvertXY = false;
+            centered();
+            if (P2.X < 0)
+            {
+                invertX();
+                InvertX = true;
+            }
+            if (P2.Y < 0)
+            {
+                invertY();
+                InvertY = true;
+            }
+            if (P2.X < P2.Y)
+            {
+                invertXY();
+                InvertXY = true;
+            }
+            BZN();
+            reverseTransformations(InvertX, InvertY,InvertXY);
+        }
+        private void reverseTransformations(bool InvertX, bool InvertY, bool InvertXY)
+        {
+            if (InvertXY)
+            {
+                invertXY();
+            }
+            if (InvertY)
+            {
+                invertY();
+            }
+            if (InvertX)
+            {
+                invertX();
+            }
+            reverseCentered();
+        }
+        private void BZN()
+        {
+            float x = P1.X, y = P1.Y;
+            float delX = P2.X - P1.X;
+            float delY = P2.Y - P1.Y;
+            float e = 2 * delY - delX;
+            for (int i = 0; i < delX; i++)
+            {
+                pixels.Add(new PointF(x, y));
+                while (e >= 0)
+                {
+                    y += 1;
+                    e = e - 2 * delX;
+                }
+                x += 1;
+                e = e + 2 * delY;
+            }
+            pixels.Add(new PointF(P2.X, P2.Y));
+        }
+        private void centered()
+        {
+            centeredX = -P1.X;
+            centeredY = -P1.Y;
+            P1.X = 0;
+            P1.Y = 0;
+            P2.X += centeredX;
+            P2.Y += centeredY;
+        }
+        void reverseCentered()
+        {
+            P1.X = -centeredX;
+            P1.Y = -centeredY;
+            P2.X -= centeredX;
+            P2.Y -= centeredY;
+            for (int i = 0; i < pixels.Count; i++)
+            {
+                pixels[i] = new PointF(pixels[i].X - centeredX, pixels[i].Y - centeredY);
+            }
+        }
+        private void invertY()
+        {
+            P2.Y = -P2.Y;
+            for (int i = 0; i < pixels.Count; i++)
+            {
+                pixels[i] = new PointF(pixels[i].X, -pixels[i].Y);
+            }
+        }
+        private void invertX()
+        {
+            P2.X = -P2.X;
+            for (int i = 0; i < pixels.Count; i++)
+            {
+                pixels[i] = new PointF(-pixels[i].X, pixels[i].Y);
+            }
+        }
+        private void invertXY()
+        {
+
+            float tmp;
+            tmp = P2.X;
+            P2.X = P2.Y;
+            P2.Y = tmp;
+            for (int i = 0; i < pixels.Count; i++)
+            {
+                pixels[i] = new PointF(pixels[i].Y, pixels[i].X);
+            }
+        }
+        public List<PointF> GetPixels()
+        {
+            return pixels;
+        }
+    }
+}
